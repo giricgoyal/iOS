c3f0213678d6008741d2de005d0ef6e5ec3bcfa8
giricgoyal <giricgoyal@gmail.com>
2014-03-01 15:00:55 -0600

    Initial Commit

---
+++ NuPal/NuPal/NuPal/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NuPal
+//
+//  Created by Giric on 3/1/14.
+//  Copyright (c) 2014 NuPal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
